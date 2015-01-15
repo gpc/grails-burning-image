ca4c9f331d836d48e65f5e0f94383f622e00be77
Lari Hotari <lhotari@pivotal.io>
2015-01-15 08:42:12 -0500

    use https for jboss repo

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.dependency.resolution = {
         // from public Maven repositories
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "https://repository.jboss.org/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
